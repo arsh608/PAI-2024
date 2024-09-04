e1fc3608493039bf04d79a5622e15b6054c45120
Arsh Al Aman <93732057+arsh608@users.noreply.github.com>
2024-09-04 10:01:00 +0500

    Create task2.py

--- lab3/task2.py
+++ lab3/task2.py
@@ -1,12 +1,12 @@
 try:
     find= str(input("Enter the word to find: "))
     replace= str(input("Enter the word to replace with: "))
-    with open('example.txt', 'r') as file:
+    with open('task2.txt', 'r') as file:
         content = file.read()
         if find in content:
             content= content.replace(find, replace)
             file.close()
-            with open('example.txt','w') as file:
+            with open('task2.txt','w') as file:
                 file.write(content)
         else:
             print("Word not found")
