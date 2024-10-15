5db91c9941006dfc7d94829d0a54fc24cce5590a
Arsh Al Aman <93732057+arsh608@users.noreply.github.com>
2024-10-15 22:33:55 +0500

    Create functions.py

---
+++ lab1/functions.py
@@ -0,0 +1,34 @@
+1) capitalize() - Converts the first character to upper case.
+2) casefold() - Converts string into lower case.
+3) center() - Returns a centered string.
+4) count() - Returns the number of times a specified value occurs in a string.
+5) endswith() - Returns true if the string ends with the specified value.
+6) find() - Searches the string for a specified value and returns the position of where it was
+found.
+
+7) format() - Formats specified values in a string.
+8) format_map() – This method is used to format specified values in a string by using a
+mapping (usually a dictionary) to replace placeholders with corresponding values. It is
+similar to the format() method, but instead of positional or keyword arguments, it takes a
+mapping object as its argument.
+9) index() - Searches the string for a specified value and returns the position of where it was
+found.
+10) isalnum() - Returns True if all characters in the string are alphanumeric.
+11) isalpha() - Returns True if all characters in the string are in the alphabet.
+12) isdigit() - Returns True if all characters in the string are digits.
+13) islower() - Returns True if all characters in the string are lower case.
+14) isupper() - Returns True if all characters in the string are upper case.
+15) isnumeric() - Returns True if all characters in the string are numeric.
+16) isspace() - Returns True if all characters in the string are whitespaces.
+17) istitle() - Returns True if the string follows the rules of a title.
+18) join() - Joins the elements of an iterable to the end of the string.
+19) lower() - Converts a string into lower case.
+20) upper() - Converts a string into upper case.
+21) replace() - Returns a string where a specified value is replaced with a specified value.
+22) rfind()- Searches the string for a specified value and returns the last position of where it
+was found.
+23) split() - Splits the string at the specified separator, and returns a list.
+24) splitlines() - Splits the string at line breaks and returns a list.
+25) startswith() - Returns true if the string starts with the specified value.
+26) title() - Converts the first character of each word to upper case.
+27) zfill() Fills the string with a specified number of 0 values at the beginning.
